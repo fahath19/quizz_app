bd62c74217551bbedd82284a6ecb47d8709bbc4a
fahath19 <github241@gmail.com>
2023-10-03 22:18:33 +0530

    files commited

---
+++ script.js
@@ -0,0 +1,8 @@
+let menu=document.querySelector('.menu');
+let ul=document.querySelector('nav ul')
+//let showmenu=document.querySelector('.show-menu')
+
+menu.addEventListener('click' ,()=>{
+      ul.classList.toggle('show-menu');
+      console.log("df")      
+});
