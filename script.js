69b24696193407b39007391d50e77cfc8974116e
fahath19 <github241@gmail.com>
2023-10-05 21:08:51 +0530

    all files commites

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
+/*
 let menu=document.querySelector('.menu');
 let ul=document.querySelector('nav ul')
 //let showmenu=document.querySelector('.show-menu')
+let playbtn=document.querySelector('.playbtn');
+let joinbtn=document.querySelector('.joinbtn');
 
 menu.addEventListener('click' ,()=>{
       ul.classList.toggle('show-menu');
       console.log("df")      
 });
+*/
+// playbtn.addEventListener('click',()=>{
+//     document.querySelector('.container').style.display="none";
+//     document.querySelector('.joinpage').style.display="flex";
+
+// });
+// joinbtn.addEventListener('click',()=>{
+//       let input= document.querySelector('input')
+//      // console.log(input.value)
+//       if(input.value=="2003"){
+//         document.querySelector('.joinpage').style.display="none";
+//         input.value=""
+        
+//       }
+//       else{
+//             console.log('fail')
+//         input.value=""
+
+//       }
+// });
