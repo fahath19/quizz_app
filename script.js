2c5c9695b2849d2477360fb7af8309e9c29efb69
fahath19 <github241@gmail.com>
2023-10-06 22:41:23 +0530

    files update

--- script.js
+++ script.js
@@ -1,31 +1,71 @@
-/*
-let menu=document.querySelector('.menu');
-let ul=document.querySelector('nav ul')
-//let showmenu=document.querySelector('.show-menu')
-let playbtn=document.querySelector('.playbtn');
-let joinbtn=document.querySelector('.joinbtn');
+let question=[
+      {
+         qizz:"Who is the Father of our Nation?",
+         a:"Mahatma Gandhi",
+         b:" Jawaharlal Nehru ",
+         c:"Bahath Singh",
+         d:"Ambedkar",
+         ans:"a "
+  
+      },
+      {
+          qizz:"Where was India’s first national Museum opened?",
+          a:"Delhi",
+          b:"Hyderabad",
+          c:"Rajasthan",
+          d:"Mumbai",
+          ans:"d"
+   
+       },    {
+          qizz:"The world’s nation 5G mobile network was launched by which country?",
+          a:"Japan",
+          b:"Asia",
+          c:"Malaysia",
+          d:"South Korea",
+          ans:"d"
+   
+       },
+       {
+          qizz:"The green planet in the solar system is?",
+          a:"Mars",
+          b:"Uranus",
+          c:"Venus",
+          d:"Earth"
+          ,
+          ans:"b"
+   
+       },
+       {
+          qizz:" Why is the color of papaya yellow?",
+          a:"Lycopene",
+          b:"Papain",
+          c:"Carotene",
+          d:"Caricaxanthin",
+          ans:"d"
+   
+       },
+  ]
+let span=document.querySelector('span');
+let nbtn=document.querySelector('.nbtn');
+let ques=document.querySelector('.ques')
+let s1=document.querySelector('.s1');
+let s2=document.querySelector('.s2');
+let s3=document.querySelector('.s3');
+let s4=document.querySelector('.s4');
 
-menu.addEventListener('click' ,()=>{
-      ul.classList.toggle('show-menu');
-      console.log("df")      
+currentquestion=0
+function load(index){
+   let data=question[index];
+    ques.innerHTML=`${index+1}.${data.qizz}`;
+     s1.innerText=`${data.a}`;
+     s2.innerText=`${data.b}`;
+     s3.innerText=`${data.c}`;
+     s4.innerText=`${data.d}`;
+
+     
+}
+load(currentquestion);
+nbtn.addEventListener('click',()=>{
+   currentquestion++;
+   load(currentquestion)
 });
-*/
-// playbtn.addEventListener('click',()=>{
-//     document.querySelector('.container').style.display="none";
-//     document.querySelector('.joinpage').style.display="flex";
-
-// });
-// joinbtn.addEventListener('click',()=>{
-//       let input= document.querySelector('input')
-//      // console.log(input.value)
-//       if(input.value=="2003"){
-//         document.querySelector('.joinpage').style.display="none";
-//         input.value=""
-        
-//       }
-//       else{
-//             console.log('fail')
-//         input.value=""
-
-//       }
-// });
